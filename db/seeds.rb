4efc4b2ee811423f093fb6640157a1ce7a66616e
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-05-27 22:24:55 +0800

    Seed member data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# seed all members data
+emails = %w(
+  xxyyzz@gmail.com
+  aabbcc@ezy.com.au
+  wwyyaa@ntu.edu.sg
+  uuttcc@gmail.com
+  funnymeh@yahoo.com.sg
+  pinkpikachu@buzz.sg
+  englishtea@gmail.com
+)
+
+emails.each{|email| Member.create(email: email)}
