92ed9ce753fecf97bce100c3c3720455a7995c80
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-05-28 14:34:57 +0800

    Update application_form factories

--- spec/factories/application_forms.rb
+++ spec/factories/application_forms.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     company { Faker::Company.name }
     message { Faker::Lorem.sentences(number: 1, supplemental: true) }
+    created_at { Faker::Date.in_date_period }
     member
   end
 end
