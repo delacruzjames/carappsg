4fcb3858c42952634047e329441b84feb15c336c
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-05-28 12:15:36 +0800

    Added rails-controller-testing gem

--- app/models/concerns/email_validation.rb
+++ app/models/concerns/email_validation.rb
@@ -2,7 +2,6 @@ module EmailValidation
   extend ActiveSupport::Concern
 
   included do
-    VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, uniqueness: true
   end
 end
